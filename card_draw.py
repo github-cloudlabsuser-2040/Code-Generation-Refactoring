035a5b6f51edccf9811292cd4d0a9836c079f798
github-cloudlabsuser-2040 <169061424+github-cloudlabsuser-2040@users.noreply.github.com>
2024-11-20 15:28:43 +0000

    CartasReparadas

--- card_draw.py
+++ card_draw.py
@@ -1,10 +1,11 @@
 # Intentionally flawed Python program
 
+
 # importing modules
 import itertools, random
 
 # make a deck of cards
-deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club'])
+deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
 
 # shuffle the cards
 random.shuffle(deck)
@@ -12,5 +13,5 @@ random.shuffle(deck)
 
 # draw five cards
 print("You got:")
-for i in range(5)
-   print(deck[i][0], "of", deck[i][1]
+for i in range(5):
+   print(deck[i][0], "of", deck[i][1])
